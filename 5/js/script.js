7da7c7f22557b8193036eb2fb10500be29d405e1
Keks <keks@htmlacademy.ru>
2023-04-25 15:40:50 +0300

    :heavy_check_mark: Сборка #5

--- 5/js/script.js
+++ 5/js/script.js
@@ -1,14 +1,30 @@
-let headerNav = document.querySelector(".header");
-let headerToggle = document.querySelector(".header__menu-toggle");
+const headerNav = document.querySelector(".header");
+const headerToggle = document.querySelector(".header__menu-toggle");
 
-headerNav.classList.remove("header--nojs");
+if (headerNav.classList.contains("header--nojs")) {
+  headerNav.classList.remove("header--nojs");
+}
 
 headerToggle.addEventListener("click", function () {
-  if (headerNav.classList.contains("header--menu-closed")) {
-    headerNav.classList.remove("header--menu-closed");
-    headerNav.classList.add("header--menu-opened");
-  } else {
-    headerNav.classList.add("header--menu-closed");
-    headerNav.classList.remove("header--menu-opened");
-  }
+  headerNav.classList.toggle("header--menu-opened");
 });
+
+// headerToggle.addEventListener("click", function () {
+//   if (headerNav.classList.contains("header--menu-closed")) {
+//     headerNav.classList.remove("header--menu-closed");
+//     headerNav.classList.add("header--menu-opened");
+//   } else {
+//     headerNav.classList.add("header--menu-closed");
+//     headerNav.classList.remove("header--menu-opened");
+//   }
+// });
+
+// Если значение переменной не меняется в скрипте, то при её объявлении используем не let, а const
+// Прежде чем вешать событие или как-то работать с элементов разметки, надо убедиться что он существует на странице, например:
+// if (headerNav) {
+//   headerNav.classList.remove("header--nojs");
+// }
+// Действия по добавлению и удалению классов -closed и -opened можно упростить при помощи функции classList.toggle(classname)
+
+// let sectionMap = document.querySelector(".main-index__map");
+// sectionMap.classList.remove("main-index__map--nojs");
